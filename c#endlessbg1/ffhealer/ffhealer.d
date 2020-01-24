f3cf34e87ef3c26d66ead54bdfbf11dad6009a60
Gitjas <Gitjas@users.noreply.github.com>
2020-01-24 20:37:43 +0100

    Flaming Fist Healer changes
    -"Flaming Fist Healer come into Undercity Temple": FF healer takes away the bodies also if PC doesn't want help with healing; FF soldiers do not walk into the temple whan they are leaving, FF healers do not do RandomWalk() while they can't see the PC.

--- c#endlessbg1/ffhealer/ffhealer.d
+++ c#endlessbg1/ffhealer/ffhealer.d
@@ -5,12 +5,18 @@ IF ~Global("C#st_SarevokHealGroup","MYAREA",0)~ THEN duke_priest
 SAY @80 /* ~<CHARNAME>! The Dukes sent us to see to your well-being. We will take care of the bodies of Sarevok and his minions.~ */
 = @81 /* ~Do you want us to heal you and disarm the traps as well?~ */
 ++ @82 /* ~Yes, please do.~ */ + duke_priest_02
-++ @83 /* ~No thanks, I'll manage on my own.~ */ + duke_priest_01
+++ @83 /* ~No thanks, I'll manage on my own.~ */ DO ~
+		ActionOverride("Sarevok",DestroySelf())
+		ActionOverride("Galdor",DestroySelf())
+		ActionOverride("Semaj",DestroySelf())
+		ActionOverride("Tazok",DestroySelf())~ + duke_priest_01
 END
 
 IF ~~ THEN duke_priest_01
 SAY @84 /* ~You did a great deed for the city. Return to the Palace and speak to the Dukes, they wish to thank you. Fare well.~ */
-IF ~~ THEN DO ~ActionOverride("c#stsdp2",EscapeArea()) ActionOverride("c#stsdpr",EscapeArea())~ EXIT
+IF ~~ THEN DO ~ActionOverride("c#stsdp2",MoveToPoint([900.1150])) 
+ActionOverride("c#stsdpr",MoveToPoint([910.1150]))
+ActionOverride("c#stsdp2",DestroySelf()) ActionOverride("c#stsdpr",DestroySelf())~ EXIT
 END 
 
 IF ~~ THEN duke_priest_02
